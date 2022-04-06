918d1da8648b8214b4f3a7e4e11d886aba8fd7a5
Mateus Camargo <mateuscamargo0@outlook.com.br>
2022-04-05 23:16:40 -0300

    Estrutura inicial para Linked List.

---
+++ linked_list.py
@@ -0,0 +1,50 @@
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+    def get_value(self):
+        return self.value
+    
+    def set_value(self, value):
+        self.value = value
+    
+    def get_next(self):
+        return self.next
+    
+    def set_next(self, value):
+        self.next = value
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def add(self, value):
+        node = Node(value)
+        node.set_next(self.head)
+        self.head = node
+
+    def size(self):
+        count = 0
+        curr = self.head
+
+        while curr != None:
+            count += 1
+            curr = curr.get_next()
+        
+        return count
+
+
+if __name__ == '__main__':
+    linked_list = LinkedList()
+    
+    for i in range(2):
+        linked_list.add(i)
+
+    linked_list.size()
