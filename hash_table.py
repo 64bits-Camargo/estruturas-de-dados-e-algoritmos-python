3b32b34176f0ab372538e44ad2e09a5844f2e106
Mateus Camargo <mateusca@bancomoneo.com>
2022-04-12 12:42:02 -0300

    Implementando "Trocando nomenclatura das tabela de hash HashTable

--- hash_table.py
+++ hash_table.py
@@ -10,42 +10,42 @@ class HashTable:
     
     def __init__(self, size=10):
         self.size = size
-        self.table =[[] for _ in range(self.size)]
+        self.tables =[[] for _ in range(self.size)]
     
     def hashing(self, key):
         return hash(key) % self.size
     
     def __setitem__(self, key, value):
         hash_key = self.hashing(key)
-        if self.table[hash_key]:
-            for index, k_value in enumerate(self.table[hash_key]):
+        if self.tables[hash_key]:
+            for index, k_value in enumerate(self.tables[hash_key]):
                 k, v = k_value
                 if key == k:
-                    self.table[hash_key][index] = ((key, value))
+                    self.tables[hash_key][index] = ((key, value))
                     break
-        self.table[hash_key].append((key, value))
+        self.tables[hash_key].append((key, value))
     
     def __getitem__(self, key):
         hash_key = self.hashing(key)
-        if self.table[hash_key]:
-            for index, k_value in enumerate(self.table[hash_key]):
+        if self.tables[hash_key]:
+            for index, k_value in enumerate(self.tables[hash_key]):
                 k, v = k_value
                 if key == k:
                     return v
     
     def __delitem__(self, key):
         hash_key = self.hashing(key)
-        if self.table[hash_key]:
-            for index, k_value in enumerate(self.table[hash_key]):
+        if self.tables[hash_key]:
+            for index, k_value in enumerate(self.tables[hash_key]):
                 k, v = k_value
                 if key == k:
-                    del self.table[hash_key][index]
+                    del self.tables[hash_key][index]
                     return
         raise KeyError
             
     def __repr__(self):
         return 'HashTable(table={result})'.\
-            format(result=json.dumps(self.table))
+            format(result=json.dumps(self.tables))
             
 
 if __name__ == '__main__':
