ce1f58bdf217865ce9f0c4ad06b6770f230ca48b
Mateus Camargo <mateusca@bancomoneo.com>
2022-04-12 11:57:43 -0300

    Implementando "magical methods" na HashTable

--- hash_table.py
+++ hash_table.py
@@ -16,39 +16,49 @@ class HashTable:
     def hashing(self, key):
         return hash(key) % self.size
     
-    def insert(self, key, value):
+    def __setitem__(self, key, value):
         hash_key = self.hashing(key)
-
+        
         if self.table[hash_key]:
             for index, k_value in enumerate(self.table[hash_key]):
                 k, v = k_value
-
                 if key == k:
-                    raise DuplicateKeyError(key)
-
+                    self.table[hash_key][index] = ((key, value))
+                    break
+                
         self.table[hash_key].append((key, value))
-
+    
+    def __getitem__(self, key):
+        hash_key = self.hashing(key)
+        if self.table[hash_key]:
+            for index, k_value in enumerate(self.table[hash_key]):
+                k, v = k_value
+                if key == k:
+                    return v
+    
     def __repr__(self):
-        return 'HashTable(table={result})'.format(result=json.dumps(self.table))
+        return 'HashTable(table={result})'.\
+            format(result=json.dumps(self.table))
+            
 
 if __name__ == '__main__':
     hash_table = HashTable()
 
     print(hash_table)
+    
+    hash_table['Nome'] = 'Mateus 1'
+    # hash_table.insert('Nome', 'Mateus 1')
+    # # hash_table.insert('Nome', 'Mateus 1') 
+    # hash_table.insert('Noma', 'Mateus 2') 
+    # hash_table.insert('Nomi', 'Mateus 3')
+    # hash_table.insert('Nomo', 'Mateus 4')
+    # hash_table.insert('Nom2', 'Mateus 5')
+    # hash_table.insert('No2m2', 'Mateus 6')
+    # hash_table.insert('No2m322', 'Mateus 326')
+    # hash_table.insert('No2m323322', 'Mate3232us 326')
+    # hash_table.insert('No32322m323322', 'Mate3232us 326')
+    # hash_table.insert('3232322', 'Mate3232us 326')
 
-
-    hash_table.insert('Nome', 'Mateus 1')
-    # hash_table.insert('Nome', 'Mateus 1') 
-    hash_table.insert('Noma', 'Mateus 2') 
-    hash_table.insert('Nomi', 'Mateus 3')
-    hash_table.insert('Nomo', 'Mateus 4')
-    hash_table.insert('Nom2', 'Mateus 5')
-    hash_table.insert('No2m2', 'Mateus 6')
-    hash_table.insert('No2m322', 'Mateus 326')
-    hash_table.insert('No2m323322', 'Mate3232us 326')
-    hash_table.insert('No32322m323322', 'Mate3232us 326')
-    hash_table.insert('3232322', 'Mate3232us 326')
-
-    hash_table.insert('3232DSDS322', 'Mate3DSSD232us 326')
+    # hash_table.insert('3232DSDS322', 'Mate3DSSD232us 326')
 
     print(hash_table)
